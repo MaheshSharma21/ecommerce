5bb0bd615a1d11f3e59dc1832cad3c9dc3af93d6
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-01-29 19:03:36 +0530

    project 2 by mahesh

---
+++ src/main/java/com/BikkadIT/Github2Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Github2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/Github2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Github2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
